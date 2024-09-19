cfb58a7ad6c6f5f6e40f9b6020ea9f43587395e7
Alfie Tham <20981237+alfietham@users.noreply.github.com>
2024-09-19 09:23:36 -0400

    chore: cleanup templated App.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders Weather Forecast header', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/Weather Forecast/i)
+  expect(linkElement).toBeInTheDocument()
+})
